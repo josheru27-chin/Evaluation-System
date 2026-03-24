e2c8cd1b52f6f223f7c386a7176ba29cd651d487
josheru27-chin <jochellearon@gmail.com>
2026-03-24 13:17:04 +0800

    fix live apply to all setting

--- MyApp/urls.py
+++ MyApp/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Evaluator
     path('', views.eval_login, name='eval_login'),
     path('form/', views.eval_forms, name='eval_forms'),
+
+    # Custom Admin
+    path('dashboard/login/', views.admin_login, name='admin_login'),
+    path('dashboard/base/', views.admin_base, name='admin_base'),
+    path('dashboard/department/', views.admin_department, name='admin_department'),
+    path('dashboard/manage/', views.admin_manage, name='admin_manage'),
+    path('dashboard/overall/', views.admin_overall, name='admin_overall'),
+    path('dashboard/sidebar/', views.admin_sidebar, name='admin_sidebar'),
+
+    # Head
+    path('head/add/', views.head_add, name='head_add'),
+    path('head/monitor/', views.head_monitor, name='head_monitor'),
 ]
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 
+
+# Evaluator-----------------------------------------------------------------------------------------
+
 def eval_login(request):
-    return render(request, 'eval_login.html')
+    return render(request, 'Evaluator/eval_login.html')
 
 def eval_forms(request):
-    return render(request, 'eval_forms.html')
+    return render(request, 'Evaluator/eval_forms.html')
+
+
+# Admin---------------------------------------------------------------------------------------------
+
+def admin_login(request):
+    return render(request, 'Admin/admin_login.html')
+
+def admin_base(request):
+    return render(request, 'Admin/admin_base.html')
+
+def admin_department(request):
+    return render(request, 'Admin/admin_department.html')
+
+def admin_manage(request):
+    return render(request, 'Admin/admin_manage.html')
+
+def admin_overall(request):
+    return render(request, 'Admin/admin_overall.html')
+
+def admin_sidebar(request):
+    return render(request, 'Admin/admin_sidebar.html')
+
+
+
+# Head----------------------------------------------------------------------------------------------
+
+def head_add(request):
+    return render(request, 'Head/head_add.html')
+
+def head_monitor(request):
+    return render(request, 'Head/head_monitor.html')
